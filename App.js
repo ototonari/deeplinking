c5e675371872dbf03e95850b16bd02850e7f0a5a
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-06-07 01:49:03 +0900

    add Twitter login function

--- App.js
+++ App.js
@@ -1,101 +1,49 @@
+'use strict';
+
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity,Linking } from 'react-native';
-import Expo from "expo";
+import { Provider } from "react-redux";
+import store from "./src/store";
+import { AppLoading, Asset, Font, Constants } from 'expo'
+
+// import start component
+import AppRouter from "./src/Router";
+
+/* npm */
+import twitter from 'react-native-simple-twitter'
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      link: '',
-      params: ''
+      isLoadingComplete: false
     }
   }
-
-  async componentDidMount() {
-    // コンポーネント描画後にリスナーを登録する。
-    // こちらはアプリが既に起動されていてdeeplinkの情報を取得したい場合
-    Linking.addEventListener('url', this._callback)
-
-    // こちらはアプリが未起動で、deeplinkによってアプリが起動された場合に、その情報を取得する場合
-    this._getParams()
-  }
-
-  _callback(value) {
-    console.warn(value)
-  }
-
-  async _getParams() {
-    // Deeplinkによってアプリが起動した場合、そのリンク情報を取得する。
-    const result = await Expo.Linking.parseInitialURLAsync()
-    console.warn(result)
-  }
-
-  _renderButton = (label, callback) => (
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => {
-        if (callback) {
-          callback()
-        }
-      }} >
-      <Text>{label}</Text>
-    </TouchableOpacity>
-  )
-
 
   render() {
-    // 取得するタイミングは任意で可能
-    const showParams = async () => {
-      const result = await Expo.Linking.parseInitialURLAsync()
-      console.warn(result)
-      this.setState({ params: JSON.stringify(result)})
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={(error) => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      )
     }
-
-    // callbackのリダイレクト先のdeeplinkを生成する。
-    const makeUrl = () => {
-      // callback元のpath
-      const path = 'https://qpit.herokuapp.com/auth/twitter' // Maybe
-      // アプリに渡したいクエリー情報を定義する
-      const queryParams = {
-        hoge: 'bar'
-      }
-      // Browser からアプリにリダイレクトする為のLink
-      const deeplink = Expo.Linking.makeUrl(path, queryParams)
-      console.warn("deeplink: ", deeplink)
-      this.setState({ link: deeplink })
+    else {
+      return (
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
+      )
     }
-
-    return (
-      <View style={styles.container}>
-        <Text>生成された値はコピペ可能</Text>
-        <Text>Link:</Text>
-        <Text selectable={true} >{ this.state.link }</Text>
-        <Text></Text>
-        <Text>Params:</Text>
-        <Text selectable={true} >{ this.state.params }</Text>
-        { this._renderButton('show params', () => showParams() ) }
-        { this._renderButton('make deeplink', () => makeUrl() ) }
-      </View>
-    );
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      twitter.setConsumerKey(Constants.manifest.extra.twitter.consumerKey, Constants.manifest.extra.twitter.consumerKeySecret)
+    ])
+  };
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    margin: 10,
-    borderWidth: 1,
-    borderRadius: 20,
-    borderColor: '#009FE8',
-    padding: 3,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    height: 30,
-  },
-
-});
